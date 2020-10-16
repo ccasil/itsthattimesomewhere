8489538f3c9f16d88b27abd7c07bfaf0426a9504
ccasil <cesarkylecasil@gmail.com>
2020-10-15 21:20:23 -0700

    js practice

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
 console.log('Client-side code running');
 
 const button = document.getElementById('time-btn');
+let current = new Date().getTime()
+
 button.addEventListener('click', function(e) {
   console.log('button was clicked');
-    document.getElementById('time').innerHTML = new Date().toString();
+    // document.getElementById('time').innerHTML = (3600000 - current % 3600000).toString();
 });
+
+setInterval(function(){
+  const newnum = (3600000 - current % 3600000).toString()
+  document.getElementById('time').innerHTML = newnum;
+ }, 100);
+
+const textcol = document.getElementById('newtextcolor');
+textcol.className = "text-success";
