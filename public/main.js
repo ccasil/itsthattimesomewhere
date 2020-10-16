a34b74b30a882e5cac88595cb1c798a9aa1e25f6
ccasil <cesarkylecasil@gmail.com>
2020-10-15 23:08:27 -0700

    working to :20

--- public/main.js
+++ public/main.js
@@ -1,17 +1,39 @@
 console.log('Client-side code running');
 
 const button = document.getElementById('time-btn');
-let current = new Date().getTime()
+calculatehours();
+setInterval(function () {
+  let currentminutes = calculateminutes();
+  let currentseconds = (60-(new Date().getSeconds())).toString();
+  const newnum = currentminutes + ' minutes and ' + currentseconds + ' seconds until the next x:20';
+  document.getElementById('time').innerText = newnum;
+  console.log(newnum);
+  return newnum;
+  // document.getElementById('time').innerHTML = newnum;
+}, 1000);
 
-button.addEventListener('click', function(e) {
-  console.log('button was clicked');
-    // document.getElementById('time').innerHTML = (3600000 - current % 3600000).toString();
+function calculateminutes() {
+  let mintemp = (new Date().getMinutes()-20);
+  console.log(mintemp);
+  if (mintemp > 30) {
+    mintemp = mintemp - 60;
+  }
+  return Math.abs(mintemp).toString();
+};
+
+function calculatehours() {
+  // 04 or 16
+  let hourtemp = (new Date().getHours());
+  // if 04 to 15
+  // if 16 to 24 && 00 to 03
+  console.log(hourtemp);
+  return hourtemp.toString();
+}
+
+
+button.addEventListener('click', function (e) {
+  console.log('button was clicked', newnum);
 });
-
-setInterval(function(){
-  const newnum = (3600000 - current % 3600000).toString()
-  document.getElementById('time').innerHTML = newnum;
- }, 100);
 
 const textcol = document.getElementById('newtextcolor');
 textcol.className = "text-success";
